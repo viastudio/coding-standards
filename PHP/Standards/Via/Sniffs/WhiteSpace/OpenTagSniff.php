943c6ed84747d7590d7dc2b231070ed33e072e61
Jason McCreary <jason@pureconcepts.net>
2014-03-14 16:23:59 -0400

    Added whitespace sniff for PHP open tag.

---
+++ PHP/Standards/Via/Sniffs/WhiteSpace/OpenTagSniff.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Via_Sniffs_WhiteSpace_OpenTagSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006-2012 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * Via_Sniffs_WhiteSpace_OpenTagSniff.
+ *
+ * Ensure there's no white space before the PHP open tag when on its own line.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006-2012 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ * @version   Release: 1.5.2
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class Via_Sniffs_WhiteSpace_OpenTagSniff implements PHP_CodeSniffer_Sniff {
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register() {
+        return array(T_OPEN_TAG);
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param integer              $stackPtr  The position of the current token in the
+     *                                        stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr) {
+        $tokens = $phpcsFile->getTokens();
+
+        if ($stackPtr === 0) {
+            return;
+        }
+
+        $previous_content = $tokens[$stackPtr - 1]['content'];
+        $trim_len = strlen(trim($previous_content));
+
+        if ($trim_len === 0) {
+            $error = 'Opening PHP tag must not be indented when on its own line';
+            $phpcsFile->addError($error, $stackPtr, 'OpenTag', array());
+        }
+    }
+}
