0fee43f36dbaf29695c5cb7d02f62959979060e0
Jason McCreary <jason@pureconcepts.net>
2014-05-16 16:03:42 -0400

    Customized configuration for Generic.WhiteSpace.ScopeIndent.

--- php/phpcs/Standards/Via/Sniffs/WhiteSpace/OpenTagSniff.php
+++ php/phpcs/Standards/Via/Sniffs/WhiteSpace/OpenTagSniff.php
@@ -32,7 +32,7 @@ class Via_Sniffs_WhiteSpace_OpenTagSniff implements PHP_CodeSniffer_Sniff {
      * @return array
      */
     public function register() {
-        return array(T_OPEN_TAG);
+        return array(T_OPEN_TAG,T_CLOSE_TAG);
     }
 
     /**
@@ -51,10 +51,22 @@ class Via_Sniffs_WhiteSpace_OpenTagSniff implements PHP_CodeSniffer_Sniff {
             return;
         }
 
+        if ($tokens[$stackPtr - 1]['line'] !== $tokens[$stackPtr]['line']) {
+            return;
+        }
+
         $previous_content = $tokens[$stackPtr - 1]['content'];
         $trim_len = strlen(trim($previous_content));
 
-        if ($trim_len === 0) {
+        if ($tokens[$stackPtr]['code'] === T_CLOSE_TAG) {
+            $lastContent = $phpcsFile->findFirstOnLine(T_OPEN_TAG, $stackPtr);
+            if (!$lastContent && $trim_len === 0) {
+                $error = 'Closing PHP tag must not be indented when on its own line';
+                $phpcsFile->addError($error, $stackPtr, 'CloseTag', array());
+                return;
+            }
+        }
+        elseif ($tokens[$stackPtr]['code'] === T_OPEN_TAG && $trim_len === 0) {
             $error = 'Opening PHP tag must not be indented when on its own line';
             $phpcsFile->addError($error, $stackPtr, 'OpenTag', array());
         }
